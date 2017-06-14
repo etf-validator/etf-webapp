3872130f0c48b9850779ce6b5f528fa01a4a1ef9
Jon Herrmann <herrmann@interactive-instruments.de>
2017-06-14 17:48:40 +0200

    ETS documentation

--- docs/convert_docs.sh
+++ docs/convert_docs.sh
@@ -7,4 +7,5 @@ index.adoc \
 User_manuals/Simplified_workflows.adoc \
 Admin_manuals/index.adoc \
 Developer_manuals/WEB-API.adoc \
-Developer_manuals/Building.adoc
+Developer_manuals/Building.adoc \
+Developer_manuals/Developing_Executable_Test_Suites.adoc
