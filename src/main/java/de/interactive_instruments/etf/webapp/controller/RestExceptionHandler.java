c9f97199b73d1a067bb41725a06c05154300b539
Jon Herrmann <herrmann@interactive-instruments.de>
2019-01-13 17:26:47 +0100

    Apply new codestyle
    Signed-off-by: Jon Herrmann <herrmann@interactive-instruments.de>
    Signed-off-by: Carlos Palma <carlospalma@guadaltel.com>

--- src/main/java/de/interactive_instruments/etf/webapp/controller/RestExceptionHandler.java
+++ src/main/java/de/interactive_instruments/etf/webapp/controller/RestExceptionHandler.java
@@ -49,71 +49,71 @@ import de.interactive_instruments.exceptions.ObjectWithIdNotFoundException;
 @ControllerAdvice(annotations = {RestController.class, Component.class})
 class RestExceptionHandler {
 
-	@Autowired
-	private ApplicationContext applicationContext;
+    @Autowired
+    private ApplicationContext applicationContext;
 
-	@Autowired
-	private StatusController statusController;
+    @Autowired
+    private StatusController statusController;
 
-	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+    private final static org.slf4j.Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
 
-	private static byte[] reserve = new byte[30 * 1024 * 1024]; // Reserve 30 MB
+    private static byte[] reserve = new byte[30 * 1024 * 1024]; // Reserve 30 MB
 
-	@ExceptionHandler(value = Exception.class)
-	@ResponseBody
-	public ApiError defaultErrorHandler(final HttpServletRequest request, final HttpServletResponse response,
-			final Exception exception) {
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ApiError defaultErrorHandler(final HttpServletRequest request, final HttpServletResponse response,
+            final Exception exception) {
 
-		if (exception != null && exception.getCause() instanceof OutOfMemoryError) {
-			// Recover the reserved memory
-			reserve = null;
-			System.gc();
-			statusController.triggerMaintenance();
-			return new ApiError(exception, request.getRequestURL().toString(), applicationContext);
-		} else if (Objects.equals(exception.getMessage(), "No space left on device")
-				|| exception.getCause() != null && exception.getCause() instanceof IOException &&
-						Objects.equals(exception.getCause().getMessage(), "No space left on device")) {
-			statusController.triggerMaintenance();
-			return new ApiError(exception, request.getRequestURL().toString(), applicationContext);
-		} else if (exception.getCause() != null && exception.getCause() instanceof StackOverflowError
-				|| exception.getCause() instanceof StackOverflowError) {
-			statusController.triggerMaintenance();
-			return new ApiError(exception, request.getRequestURL().toString(), applicationContext);
-		}
-		final int status;
-		final ApiError conv;
-		if (exception instanceof IllegalEidException) {
-			status = HttpServletResponse.SC_BAD_REQUEST;
-			conv = null;
-		} else if (exception instanceof ObjectWithIdNotFoundException) {
-			status = HttpServletResponse.SC_NOT_FOUND;
-			conv = null;
-		} else if (exception.getCause() instanceof LocalizableApiError) {
-			status = ((LocalizableApiError) exception.getCause()).getStatus();
-			conv = null;
-		} else if (exception.getCause() instanceof JsonMappingException) {
-			final LocalizableApiError e = new LocalizableApiError((JsonMappingException) exception.getCause());
-			conv = new ApiError(e, request.getRequestURL().toString(), applicationContext);
-			status = e.sc;
-		} else if (exception.getCause() instanceof JsonParseException) {
-			final LocalizableApiError e = new LocalizableApiError((JsonParseException) exception.getCause());
-			conv = new ApiError(e, request.getRequestURL().toString(), applicationContext);
-			status = e.sc;
-		} else if (exception instanceof HttpMessageNotReadableException) {
-			final LocalizableApiError e = new LocalizableApiError((HttpMessageNotReadableException) exception);
-			conv = new ApiError(e, request.getRequestURL().toString(), applicationContext);
-			status = e.sc;
-		} else if (exception instanceof FileUploadBase.SizeLimitExceededException) {
-			final LocalizableApiError e = new LocalizableApiError((FileUploadBase.SizeLimitExceededException) exception);
-			conv = new ApiError(e, request.getRequestURL().toString(), applicationContext);
-			status = e.sc;
-		} else {
-			status = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
-			conv = null;
-		}
-		final ApiError apiError = conv != null ? conv
-				: new ApiError(exception, request.getRequestURL().toString(), applicationContext);
-		response.setStatus(status);
-		return apiError;
-	}
+        if (exception != null && exception.getCause() instanceof OutOfMemoryError) {
+            // Recover the reserved memory
+            reserve = null;
+            System.gc();
+            statusController.triggerMaintenance();
+            return new ApiError(exception, request.getRequestURL().toString(), applicationContext);
+        } else if (Objects.equals(exception.getMessage(), "No space left on device")
+                || exception.getCause() != null && exception.getCause() instanceof IOException &&
+                        Objects.equals(exception.getCause().getMessage(), "No space left on device")) {
+            statusController.triggerMaintenance();
+            return new ApiError(exception, request.getRequestURL().toString(), applicationContext);
+        } else if (exception.getCause() != null && exception.getCause() instanceof StackOverflowError
+                || exception.getCause() instanceof StackOverflowError) {
+            statusController.triggerMaintenance();
+            return new ApiError(exception, request.getRequestURL().toString(), applicationContext);
+        }
+        final int status;
+        final ApiError conv;
+        if (exception instanceof IllegalEidException) {
+            status = HttpServletResponse.SC_BAD_REQUEST;
+            conv = null;
+        } else if (exception instanceof ObjectWithIdNotFoundException) {
+            status = HttpServletResponse.SC_NOT_FOUND;
+            conv = null;
+        } else if (exception.getCause() instanceof LocalizableApiError) {
+            status = ((LocalizableApiError) exception.getCause()).getStatus();
+            conv = null;
+        } else if (exception.getCause() instanceof JsonMappingException) {
+            final LocalizableApiError e = new LocalizableApiError((JsonMappingException) exception.getCause());
+            conv = new ApiError(e, request.getRequestURL().toString(), applicationContext);
+            status = e.sc;
+        } else if (exception.getCause() instanceof JsonParseException) {
+            final LocalizableApiError e = new LocalizableApiError((JsonParseException) exception.getCause());
+            conv = new ApiError(e, request.getRequestURL().toString(), applicationContext);
+            status = e.sc;
+        } else if (exception instanceof HttpMessageNotReadableException) {
+            final LocalizableApiError e = new LocalizableApiError((HttpMessageNotReadableException) exception);
+            conv = new ApiError(e, request.getRequestURL().toString(), applicationContext);
+            status = e.sc;
+        } else if (exception instanceof FileUploadBase.SizeLimitExceededException) {
+            final LocalizableApiError e = new LocalizableApiError((FileUploadBase.SizeLimitExceededException) exception);
+            conv = new ApiError(e, request.getRequestURL().toString(), applicationContext);
+            status = e.sc;
+        } else {
+            status = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+            conv = null;
+        }
+        final ApiError apiError = conv != null ? conv
+                : new ApiError(exception, request.getRequestURL().toString(), applicationContext);
+        response.setStatus(status);
+        return apiError;
+    }
 }
--- src/main/java/de/interactive_instruments/etf/webapp/dto/ApiError.java
+++ src/main/java/de/interactive_instruments/etf/webapp/dto/ApiError.java
@@ -38,87 +38,87 @@ import io.swagger.annotations.ApiModelProperty;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ApiError {
 
-	private static final Logger logger = LoggerFactory.getLogger(ApiError.class);
+    private static final Logger logger = LoggerFactory.getLogger(ApiError.class);
 
-	@XmlElement(name = "error")
-	@ApiModelProperty(value = "Error message", example = "Error message")
-	private final String error;
+    @XmlElement(name = "error")
+    @ApiModelProperty(value = "Error message", example = "Error message")
+    private final String error;
 
-	@ApiModelProperty(value = "Timestamp in milliseconds, measured between the time the error occurred "
-			+ "and midnight, January 1, 1970 UTC(coordinated universal time).", example = "1488469744783")
-	@XmlElement(name = "timestamp")
-	private final String timestamp = String.valueOf(System.currentTimeMillis());
+    @ApiModelProperty(value = "Timestamp in milliseconds, measured between the time the error occurred "
+            + "and midnight, January 1, 1970 UTC(coordinated universal time).", example = "1488469744783")
+    @XmlElement(name = "timestamp")
+    private final String timestamp = String.valueOf(System.currentTimeMillis());
 
-	@ApiModelProperty(value = "URL that was invoked before the error occured", example = "http://localhost:8080/v2/X")
-	@XmlElement(name = "url")
-	private final String url;
+    @ApiModelProperty(value = "URL that was invoked before the error occured", example = "http://localhost:8080/v2/X")
+    @XmlElement(name = "url")
+    private final String url;
 
-	@ApiModelProperty(value = "Optional error ID which was used to translate the error message", example = "l.invalid.fooBar")
-	@XmlElement(name = "id")
-	private final String id;
+    @ApiModelProperty(value = "Optional error ID which was used to translate the error message", example = "l.invalid.fooBar")
+    @XmlElement(name = "id")
+    private final String id;
 
-	@ApiModelProperty(value = "Optional stacktrace which will only be attached in ETF development mode")
-	@XmlElementWrapper(name = "stacktrace")
-	@XmlElement(name = "trace")
-	private final String[] stacktrace;
+    @ApiModelProperty(value = "Optional stacktrace which will only be attached in ETF development mode")
+    @XmlElementWrapper(name = "stacktrace")
+    @XmlElement(name = "trace")
+    private final String[] stacktrace;
 
-	private ApiError() {
-		// Ctor for JAXB
-		error = null;
-		url = null;
-		id = null;
-		stacktrace = null;
-	}
+    private ApiError() {
+        // Ctor for JAXB
+        error = null;
+        url = null;
+        id = null;
+        stacktrace = null;
+    }
 
-	public ApiError(final Throwable e, final String url, final ApplicationContext applicationContext) {
-		logger.error("EXID-" + timestamp + ": An exception occurred while trying to invoke \"" +
-				url + "\"", e);
-		final LocalizableApiError localizableApiError;
-		if (e instanceof LocalizableApiError) {
-			localizableApiError = (LocalizableApiError) e;
-		} else if (e.getCause() instanceof LocalizableApiError) {
-			localizableApiError = (LocalizableApiError) e.getCause();
-		} else {
-			localizableApiError = null;
-		}
-		if (localizableApiError != null) {
-			this.id = localizableApiError.getId();
-			final String err = applicationContext.getMessage(localizableApiError.getId(),
-					localizableApiError.getArgumentValueArr(), null,
-					// localizableApiError.getUserLocale());
-					LocaleContextHolder.getLocale());
-			if (err == null) {
-				// Unknown
-				this.error = ExceptionUtils.getRootCause(e).getMessage();
-			} else {
-				this.error = err;
-			}
-		} else {
-			this.id = null;
-			if (e != null) {
-				final Throwable rootCause = ExceptionUtils.getRootCause(e);
-				this.error = rootCause != null ? rootCause.getMessage() : e.getMessage();
-			} else {
-				this.error = "Internal error";
-			}
-		}
-		stacktrace = ExceptionUtils.getRootCauseStackTrace(e);
-		this.url = url;
-	}
+    public ApiError(final Throwable e, final String url, final ApplicationContext applicationContext) {
+        logger.error("EXID-" + timestamp + ": An exception occurred while trying to invoke \"" +
+                url + "\"", e);
+        final LocalizableApiError localizableApiError;
+        if (e instanceof LocalizableApiError) {
+            localizableApiError = (LocalizableApiError) e;
+        } else if (e.getCause() instanceof LocalizableApiError) {
+            localizableApiError = (LocalizableApiError) e.getCause();
+        } else {
+            localizableApiError = null;
+        }
+        if (localizableApiError != null) {
+            this.id = localizableApiError.getId();
+            final String err = applicationContext.getMessage(localizableApiError.getId(),
+                    localizableApiError.getArgumentValueArr(), null,
+                    // localizableApiError.getUserLocale());
+                    LocaleContextHolder.getLocale());
+            if (err == null) {
+                // Unknown
+                this.error = ExceptionUtils.getRootCause(e).getMessage();
+            } else {
+                this.error = err;
+            }
+        } else {
+            this.id = null;
+            if (e != null) {
+                final Throwable rootCause = ExceptionUtils.getRootCause(e);
+                this.error = rootCause != null ? rootCause.getMessage() : e.getMessage();
+            } else {
+                this.error = "Internal error";
+            }
+        }
+        stacktrace = ExceptionUtils.getRootCauseStackTrace(e);
+        this.url = url;
+    }
 
-	public String getError() {
-		return error;
-	}
+    public String getError() {
+        return error;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String[] getStacktrace() {
-		return stacktrace;
-	}
+    public String[] getStacktrace() {
+        return stacktrace;
+    }
 }
