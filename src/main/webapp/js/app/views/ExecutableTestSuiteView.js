951c1e489dc98441f3812a707d4886ebd112e871
Carlos Palma <carlospalma@guadaltel.com>
2019-05-15 19:13:21 +0200

    Added dependency tree to automatically disable selectors
    Signed-off-by: Carlos Palma <carlospalma@guadaltel.com>

--- src/main/webapp/js/app/views/ExecutableTestSuiteView.js
+++ src/main/webapp/js/app/views/ExecutableTestSuiteView.js
@@ -92,13 +92,56 @@ define([
                     e.stopPropagation();
                     e.stopImmediatePropagation();
                     e.preventDefault();
+                    markDependants(e.currentTarget.querySelectorAll("option")[1].value);
                 });
 
                 _this.container.trigger('create');
                 _this.container.listview().listview('refresh');
             });
         },
-    });
 
+    });
+    
+    function markDependants(id){
+        var model = router.executableTestSuitesView.collection.models.filter((x) => x.id==id)[0];
+        let classList = document.querySelector("option[value="+id+"]").parentElement.parentElement.classList;
+        if (typeof model.attributes.dependencies !== "undefined" && !classList.contains("ui-disabled")){
+            var id_dependency = model.attributes.dependencies.executableTestSuite.ref;
+            if(!dependenciesTree[id_dependency]){
+                dependenciesTree[id_dependency] = [];
+            }
+            let classListDependency = document.querySelector("option[value="+id_dependency+"]").parentElement.parentElement.classList;
+            let activating = classList.contains("ui-flipswitch-active");
+            if(activating){
+                dependenciesTree[id_dependency].push(id);
+            }else{
+                dependenciesTree[id_dependency].splice(dependenciesTree[id_dependency].indexOf(id),1);
+            }
+            if(activating && !classListDependency.contains("ui-flipswitch-active")){
+                classListDependency.add("ui-flipswitch-active");
+                markDependants(id_dependency);
+            }
+            if(activating && !classListDependency.contains("ui-disabled")){
+                classListDependency.add("ui-disabled");
+            }
+            if(dependenciesTree[id_dependency].length === 0){
+                classListDependency.remove("ui-flipswitch-active");
+                classListDependency.remove("ui-disabled");
+                markDependants(id_dependency);
+            }
+            let dependenciesTreeEmpty = true;
+            for (let i in dependenciesTree) {
+                dependenciesTreeEmpty &= (dependenciesTree[i].length === 0);
+            }
+            if(dependenciesTreeEmpty){
+                document.querySelectorAll("option").forEach(function(option){
+                    classListDependency = option.parentElement.parentElement.classList;
+                    classListDependency.remove("ui-disabled");
+                });
+            }
+        }
+    }
+    
     return ExecutableTestSuiteView;
 } );
+
