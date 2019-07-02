2a48e31726f3cbf2210abeda3baeed38ae548041
Carlos Palma <carlospalma@guadaltel.com>
2019-07-02 14:54:02 +0200

    Fixes codacy issue 	- Strings must use doublequote. (quotes)
    Signed-off-by: Carlos Palma <carlospalma@guadaltel.com>

--- src/main/webapp/js/app/views/ExecutableTestSuiteView.js
+++ src/main/webapp/js/app/views/ExecutableTestSuiteView.js
@@ -57,10 +57,12 @@ define([
                 classListDependency.remove("ui-disabled");
                 markDependants(idDependency);
             }
+            
             let dependenciesTreeEmpty = true;
-            for (let i in dependenciesTree) {
-                dependenciesTreeEmpty &= (dependenciesTree[i].length === 0);
+            for (dep in dependenciesTree){
+            	dependenciesTreeEmpty &= (dep.length === 0)
             }
+            
             if(dependenciesTreeEmpty){
                 document.querySelectorAll("option").forEach(function(option){
                     classListDependency = option.parentElement.parentElement.classList;
@@ -72,7 +74,7 @@ define([
 	
     var ExecutableTestSuiteView = EtfView.extend( {
 
-        el: $('#start-tests-page'),
+        el: $("#start-tests-page"),
         container : $("#executable-test-suite-listview-container"),
         template: _.template($('script#executable-test-suite-items-template').html()),
 
@@ -90,38 +92,38 @@ define([
                     _this.template({"moment": moment, "v2": v2, "collection": _this.collection.toJSON()})
                 );
 
-                $('.executable-test-suite-selection').on('change', function (e) {
+                $(".executable-test-suite-selection").on("change", function (e) {
                     var i = $(".executable-test-suite-selection option[value!='X']:selected").length;
                     if (i == 0) {
-                        $('#fadin-start-tests-button').hide('scale');
-                        $('.executable-test-suite-selection' +
+                        $("#fadin-start-tests-button").hide("scale");
+                        $(".executable-test-suite-selection" +
                             v2.getClassNamesForNotSelection(this, "test-object-type-")).parent().removeClass("ui-disabled");
                     } else if (i == 1) {
-                        $('#fadin-start-tests-button').show('scale');
-                        $('.executable-test-suite-selection' +
+                        $("#fadin-start-tests-button").show("scale");
+                        $(".executable-test-suite-selection" +
                             v2.getClassNamesForNotSelection(this, "test-object-type-")).parent().addClass("ui-disabled");
                     }
                     markDependants(e.currentTarget.querySelectorAll("option")[1].value);
                 });
 
-                $('#fadin-start-tests-button').on('click', function (e) {
-                    var u = '#start-tests?ids=';
-                    $.each($('.executable-test-suite-selection'), function () {
+                $("#fadin-start-tests-button").on("clic", function (e) {
+                    var u = "#start-tests?ids=";
+                    $.each($(".executable-test-suite-selection"), function () {
                         var val = $(this).val();
-                        if (val != 'X') {
+                        if (val != "X") {
                             u += val + ",";
                         }
                     })
                     u = u.slice(0, -1);
                     $(this).attr("href", u);
                 });
-                $('#fadin-start-tests-button').hide();
+                $("#fadin-start-tests-button").hide();
 
                 // Filter by tag class
                 _this.container.listview({
                     autodividers: true,
                     autodividersSelector: function (li) {
-                        var tag = $(li).find('.Tag');
+                        var tag = $(li).find(".Tag");
                         if (tag.length) {
                             return tag.text();
                         } else {
@@ -131,14 +133,14 @@ define([
                 });
 
                 // Stop collapsible
-                $('.ii-stop-propagation').on('click', function (e) {
+                $(".ii-stop-propagation").on("click", function (e) {
                     e.stopPropagation();
                     e.stopImmediatePropagation();
                     e.preventDefault();
                 });
                 
-                _this.container.trigger('create');
-                _this.container.listview().listview('refresh');
+                _this.container.trigger("create");
+                _this.container.listview().listview("refresh");
             });
         },
 
