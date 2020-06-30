5e47b65d62d0fc90209b54347608d6e23909ce28
Carlos Palma <carlospalma@guadaltel.com>
2020-06-30 15:11:47 +0200

    Added check on mobileRouter.js to detect existence of TestObject and redirect to dialog
    Signed-off-by: Carlos Palma <carlospalma@guadaltel.com>

--- src/main/webapp/js/app/routers/mobileRouter.js
+++ src/main/webapp/js/app/routers/mobileRouter.js
@@ -216,12 +216,13 @@ define([
             },
 
             rerunTests: function (id) {
+                var _this = this;
                 var _id = id.substring(3);
                 var tasks = {};
                 var label = {};
                 var models = this.testReportView.collection.models;
                 _.each(models, function (model) {
-                    if (!_.isUndefined(model.attributes.id) && _id == model.attributes.id) {
+                    if ((!_.isUndefined(model)) && (!_.isUndefined(model.attributes)) && (!_.isUndefined(model.attributes.id)) && _id == model.attributes.id) {
                         tasks = model.attributes.testTasks.TestTask;
                         label = model.attributes.label;
                     }
@@ -231,56 +232,77 @@ define([
                 var testObject = {};
                 var argumentList = {};
                 var collection = this.executableTestSuitesView.collection;
-                //When there is just one task.
-                if (!_.isUndefined(tasks.executableTestSuite)) {
-                    testObject = tasks.testObject;
-                    argumentList = tasks.ArgumentList;
-                    etslocal[tasks.executableTestSuite.id] = collection._byId[tasks.executableTestSuite.id];
-                    etsremote.push(tasks.executableTestSuite.id);
-                }
-                //When there are several tasks.
-                else {
-                    testObject = tasks[0].testObject;
-                    argumentList = tasks[0].ArgumentList;
-                    _.each(tasks, function (task) {
-                        if (!_.isUndefined(task) && !_.isUndefined(task.executableTestSuite) && !_.isUndefined(task.executableTestSuite.id)) {
-                            etslocal[task.executableTestSuite.id] = collection._byId[task.executableTestSuite.id];
-                            etsremote.push(task.executableTestSuite.id);
+                if (!_.isUndefined(tasks)) {
+                    //When there is just one task.
+                    if (!_.isUndefined(tasks.executableTestSuite)) {
+                        testObject = tasks.testObject;
+                        argumentList = tasks.ArgumentList;
+                        etslocal[tasks.executableTestSuite.id] = collection._byId[tasks.executableTestSuite.id];
+                        etsremote.push(tasks.executableTestSuite.id);
+                    }
+                    //When there are several tasks.
+                    else {
+                        testObject = tasks[0].testObject;
+                        argumentList = tasks[0].ArgumentList;
+                        _.each(tasks, function (task) {
+                            if ((!_.isUndefined(task)) && (!_.isUndefined(task.executableTestSuite)) && (!_.isUndefined(task.executableTestSuite.id))) {
+                                etslocal[task.executableTestSuite.id] = collection._byId[task.executableTestSuite.id];
+                                etsremote.push(task.executableTestSuite.id);
+                            }
+                        });
+                    }
+                    if (_.isUndefined(testObject)) {
+                        var ids = "ids=";
+                        if (!_.isUndefined(tasks.executableTestSuite)) {
+                            if (!_.isUndefined(tasks.executableTestSuite.id)) {
+                                ids += tasks.executableTestSuite.id;
+                            }
                         }
-                    });
-                }
-                if (testObject.hasOwnProperty("remoteResource")) {
-                    //Rerun a remote file test automatically                           
-                    //Generate a new label.
-                    var regex1 = RegExp(/\(\d+\)$/);
-                    var regex2 = RegExp(/\d+/);
-                    if (regex1.test(label)) {
-                        var stringAttempt = regex1.exec(label);
-                        var nAttempt = parseInt(regex2.exec(stringAttempt));
-                        var newNumber = nAttempt + 1;
-                        label = label.replace(regex1, "(" + newNumber + ")");
+                        else {
+                            _.each(tasks, function (task) {
+                                if ((!_.isUndefined(task.executableTestSuite)) && (!_.isUndefined(task.executableTestSuite.id))) {
+                                    ids += task.executableTestSuite.id + ",";
+                                }
+                            });
+                            ids = ids.slice(0, -1);
+                        }
+                        _this.startTestsDialog(ids);
                     }
                     else {
-                        label = label + " (1)";
-                    }
-                    var newTestObject = new v2.TestObject(new v2.Resource("data", testObject.remoteResource));
-                    var testRun = new v2.TestRun(label, etsremote, argumentList, newTestObject);
-                    $("#start-tests-confirm").addClass('ui-disabled');
-                    v2.startTestRun(testRun, function (data) {
-                        if (!_.isUndefined(data.EtfItemCollection)) {
-                            location.href = '#monitor-test-run?id=' + data.EtfItemCollection.testRuns.TestRun.id;
-                        } else {
-                            $("#start-tests-confirm").removeClass('ui-disabled');
-                            v2.apiCallError("Could not start test run: ", "Error", data);
+                        if (testObject.hasOwnProperty("remoteResource")) {
+                            //Rerun a remote file test automatically                           
+                            //Generate a new label.
+                            var regex1 = RegExp(/\(\d+\)$/);
+                            var regex2 = RegExp(/\d+/);
+                            if (regex1.test(label)) {
+                                var stringAttempt = regex1.exec(label);
+                                var nAttempt = parseInt(regex2.exec(stringAttempt));
+                                var newNumber = nAttempt + 1;
+                                label = label.replace(regex1, "(" + newNumber + ")");
+                            }
+                            else {
+                                label = label + " (1)";
+                            }
+                            var newTestObject = new v2.TestObject(new v2.Resource("data", testObject.remoteResource));
+                            var testRun = new v2.TestRun(label, etsremote, argumentList, newTestObject);
+                            $("#start-tests-confirm").addClass('ui-disabled');
+                            v2.startTestRun(testRun, function (data) {
+                                if (!_.isUndefined(data.EtfItemCollection)) {
+                                    location.href = '#monitor-test-run?id=' + data.EtfItemCollection.testRuns.TestRun.id;
+                                } else {
+                                    $("#start-tests-confirm").removeClass('ui-disabled');
+                                    v2.apiCallError("Could not start test run: ", "Error", data);
+                                }
+                            }, function (data) {
+                                $("#start-tests-confirm").removeClass('ui-disabled');
+                            });
                         }
-                    }, function (data) {
-                        $("#start-tests-confirm").removeClass('ui-disabled');
-                    });
-                }
-                else {
-                    //Rerun a local file test 
-                    this.showDialog(this.startTestView, { executableTestSuites: etslocal });
-                    $("#start-tests-dialog > div > div[data-role='header'] a[role='button']").attr('href', '/#start-tests');
+                        else {
+                            //Rerun a local file test
+                            this.showDialog(this.startTestView, { executableTestSuites: etslocal });
+                            $("#start-tests-dialog > div > div[data-role='header'] a[role='button']").attr('href', '/#start-tests');
+                        }
+                    }
                 }
             },
 
